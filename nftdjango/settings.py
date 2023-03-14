61afd839a9f764f14d9c2d3fb3b6a21636651af5
DCK <dogancihatkayanews@gmail.com>
2023-03-14 22:26:20 +0300

    allowed hosts güncellendi

--- nftdjango/settings.py
+++ nftdjango/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-cci7c8t_ew@lqtt_+n)i2sqs2-4d7edn*ynqr&1jqan8)_p1i@'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production!!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
