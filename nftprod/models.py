32818aee5540115810513403907f55683613f82e
DCK <dogancihatkayanews@gmail.com>
2023-03-15 00:19:52 +0300

    genel aktif pasif geldi

--- nftprod/models.py
+++ nftprod/models.py
@@ -59,6 +59,7 @@ class Property(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(unique=True, editable=False)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
