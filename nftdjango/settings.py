1fde4c6e7b975278bf1da00ad1a700c3412dd0d9
DCK <dogancihatkayanews@gmail.com>
2023-03-14 22:44:19 +0300

    statict ler güncellendi

--- nftdjango/settings.py
+++ nftdjango/settings.py
@@ -178,10 +178,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-# Media ayarları
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Media settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'cnd-data')
 MEDIA_URL = '/cnd-data/'
+
 
 
 def upload_to(instance, filename):
