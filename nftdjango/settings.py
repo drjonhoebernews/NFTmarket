0bca3ba0fa257204fd82ef37d7e14e643abb0724
DCK <dogancihatkayanews@gmail.com>
2023-03-14 22:56:21 +0300

    dil ve saat sistemi güncellendi static üzerine çalışılıyor

--- nftdjango/settings.py
+++ nftdjango/settings.py
@@ -179,7 +179,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
+# https://docs.djangoproject.com/en/3.1/howto/static-files/..
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
